11e1cf13947927fd0414dc65cd12f5dc123f8e4c
Noah Fields <nfields@protonmail.com>
2021-04-14 15:07:46 -0400

    updated gitignore

--- aaaa.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-print('1')
-subprocess.Popen(['nohup', 'python3', 'db.py', 'OPTIONHOOD'])
-print('2')
--- bbbbb.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-print('1')
-subprocess.Popen(['pkill', '-f', 'python3 db.py OPTIONHOOD'])
-print('2')
